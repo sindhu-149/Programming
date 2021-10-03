13fa6fb254c92a3378310eac1e2cb0ae57997e4d
sindhu-149 <sindhukanakarao149@gmail.com>
2021-10-03 21:42:21 +0530

    Where to place a tressure

---
+++ languages/python/Tressure_map.py
@@ -0,0 +1,10 @@
+row1=["⬜️","⬜️","⬜️"]
+row2=["⬜️","⬜️","⬜️"]
+row3=["⬜️","⬜️","⬜️"]
+map=[row1,row2,row3]
+print(f"{row1}\n{row2}\n{row3}")
+position=input("where do you want to put the tressure?  ")
+horizontal=int(position[0])
+vertical=int(position[1])
+map[horizontal-1][vertical-1]="X"
+print(f"{row1}\n{row2}\n{row3}")
