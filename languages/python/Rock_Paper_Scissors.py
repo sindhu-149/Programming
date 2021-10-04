78486a56df94a1fc0ef60f5af253017275c80ccf
sindhu-149 <sindhukanakarao149@gmail.com>
2021-10-04 21:21:28 +0530

    rock paper scissors (3rd commit)

--- languages/python/Rock_Paper_Scissors.py
+++ languages/python/Rock_Paper_Scissors.py
@@ -32,20 +32,22 @@ user_choose=int(input("What do you want to choose? Type 0 for Rock, 1 for Paper 
 if user_choose>2 or user_choose<0:
   print("You have entered an invalid number. You lose!")
 else:
-  print("You choose\n")
+  print("You choose:\n")
   print(images[user_choose])
   computer_choose=random.randint(0,2)
   #we observed from the given condition larger no. wins against samller no. except in case of rock(0) and scissors(2) -- rock(0) wins
   #if user chooses rock against scissor or  user_choose is greater than computer he wins
   if user_choose==0 and computer_choose==2 or user_choose>computer_choose:
-    print("computer choose:")
+    print("computer choose:\n")
     print(images[computer_choose])
-    print("You Won")
+    print("\nYou Win")
   #if computer chooses rock against scissor or  user_choose is lessthan computer user will lose
   elif computer_choose==2 and user_choose==0 or computer_choose>user_choose:
     print("computer choose:\n")
     print(images[computer_choose])
-    print("\nYou lost")
+    print("\nYou lose")
   elif computer_choose==user_choose:
+    print("computer choose:\n")
+    print(images[computer_choose])
     print("It's a draw")
 
