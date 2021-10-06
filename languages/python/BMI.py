31e38f831f326a5751f3910ffbe90ab0221441d0
sindhu-149 <sindhukanakarao149@gmail.com>
2021-10-06 19:49:15 +0530

    body mass index

---
+++ languages/python/BMI.py
@@ -0,0 +1,7 @@
+# program to find th body mass index ,formula : weight/height^2
+height= input("Enter your height in meters: ")
+weight=input("Enter your weight in kilo grams:  ")
+# here weight is convering into 'int' type where as height is converting in float  because by default the input assumes any value ant takes it as string  (here height should of type float only )  
+BMI = int(weight)/float(height)**2
+# conerting BMI into 'int' gives the round values
+print(int(BMI))
