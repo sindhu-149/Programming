c6a8ba8ccde1b45c8fa1aac502bf9fe0d7204e0d
sindhu-149 <sindhukanakarao149@gmail.com>
2021-10-06 20:31:56 +0530

    BMI calculator

--- languages/python/BMI.py
+++ languages/python/BMI.py
@@ -3,5 +3,5 @@ height= input("Enter your height in meters: ")
 weight=input("Enter your weight in kilo grams:  ")
 # here weight is convering into 'int' type where as height is converting in float  because by default the input assumes any value ant takes it as string  (here height should of type float only )  
 BMI = int(weight)/float(height)**2
-# conerting BMI into 'int' gives the round values
+# converting BMI into 'int' gives the round values
 print(int(BMI))
