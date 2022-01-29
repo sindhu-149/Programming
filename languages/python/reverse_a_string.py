e6f9a4223ef0e9e90ec8f37ba3da57643457f5aa
sindhu-149 <82016686+sindhu-149@users.noreply.github.com>
2022-01-29 11:04:38 +0530

    Update reverse_a_string.py

--- languages/python/reverse_a_string.py
+++ languages/python/reverse_a_string.py
@@ -1,1 +1,15 @@
-reverse a string without changing the position of special charecters.
+# reverse a string without changing the position of special charecters.
+n=input()
+d=dict()
+n=list(n)
+l=[]
+for i in range(len(n)):
+  if n[i].isalpha():
+    l.append(n[i])
+  else:
+    d[i]=n[i]
+l.reverse()
+for i in d:
+  l.insert(i,d[i])
+s="".join(l)
+print(s)
