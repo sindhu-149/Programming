3608125af10dbac97d1c7d3b60e36ed31366bded
sindhu-149 <82016686+sindhu-149@users.noreply.github.com>
2022-01-30 12:46:59 +0530

    Update password_generater_2.py

--- languages/python/password_generater_2.py
+++ languages/python/password_generater_2.py
@@ -1,18 +1,14 @@
-'''
-   Given input of array of string in format <emnp name> <emp number> separated by comas ,Emp
-should contain only alphabets and employee number. You have to generate password for
-Ex:
-input: Robert: 36787, Tina: 68721, Jo:56389
-Output: tiX
-Conditions: len of robert is 6 and 6 is present in emp number
-robert (36787), so return the alphabet at position 6 that is t.
-Now len of tina is 4 and 3 is not present in the 68721 so select the number which is max and less than the len of tina so select 2 return the alphabet that is at position 2 that is i.
-Now In of Jo is 2 it is not present in 56389 and there is not present any number which is less than 2 so return X.
-
-'''
+# Given input of array of string in format <emnp name> <emp number> separated by comas ,Emp
+# should contain only alphabets and employee number. You have to generate password for
+# Ex:
+# input: Robert: 36787, Tina: 68721, Jo:56389
+# Output: tiX
+# Conditions: len of robert is 6 and 6 is present in emp number
+# robert (36787), so return the alphabet at position 6 that is t.
+# Now len of tina is 4 and 3 is not present in the 68721 so select the number which is max and less than the len of tina so select 2 return the alphabet that is at position 2 that is i.
+# Now In of Jo is 2 it is not present in 56389 and there is not present any number which is less than 2 so return X.
 
 
-'''python
 n=input().split(",")
 print(n)
 name=[]
@@ -41,4 +37,3 @@ for i in range(len(name)):
           password+="X"
 print(password)
 
-'''
